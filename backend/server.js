bed8feae3aad050665780b0b7ad06b6fad5a4212
Gabriel Matos <matos7771@hotmail.com>
2025-06-23 17:25:30 -0300

    localhost and tests

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const { createServer } = require('node:http');
 
-const host = '127.0.0.1';
+const host = 'localhost';
 const port = 3000;
 
 const server = createServer((request, response) => {
+
+    console.log("Method: " + request.method);
+    console.log("URL: " + request.url);
+
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.end("part of the browser");
 });
