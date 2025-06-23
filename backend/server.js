b1b12dbafe36156c679090e406c4f6f474688bb3
Gabriel Matos <matos7771@hotmail.com>
2025-06-23 19:02:22 -0300

    tests with routes

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,53 @@ const { createServer } = require('node:http');
 const host = 'localhost';
 const port = 3000;
 
+const products = [
+    {
+        id: 1,
+        name: "chinela",
+        price: 39.99
+    },
+    {
+        id: 2,
+        name: "alpercata",
+        price: 99.90
+    }
+];
+
 const server = createServer((request, response) => {
 
-    console.log("Method: " + request.method);
-    console.log("URL: " + request.url);
+    const { url, method } = request;
 
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end("part of the browser");
+    if(url == "/"){
+        response.writeHead(200, { 'Content-type': 'text/plain' });
+        return response.end("home page");
+    }
+       
+    if(url == "/products"){
+        response.writeHead(200, { 'Content-type': 'application/json'});
+        return response.end(JSON.stringify(products));
+    } 
+
+    if(url == "/products/add"){
+        products.push({
+            id: 3,
+            name: "ventilador",
+            price: 200.00
+        });
+
+        response.writeHead(200, { 'Content-type': 'text/plain'});
+        return response.end("Product added.");
+    } 
+
+    if(url == "/products/rem"){
+        products.pop();
+        response.writeHead(200, { 'Content-type': 'text/plain'});
+        return response.end("Product removed.");
+    } 
+
+    response.writeHead(404, { 'Content-type': 'text/plain'});
+    return response.end("not found.");
+
 });
 
 server.listen(port, host, () => {
