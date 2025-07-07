fb36c4bfc6fb2570b5a53aceabe90f231225b613
Gabriel Matos <matos7771@hotmail.com>
2025-07-07 11:26:21 -0300

    UserModel created

---
+++ backend/models/UsersModel.js
@@ -0,0 +1,23 @@
+class UserModel{
+    findAll(){
+        
+    }
+
+    findByID(){
+
+    }
+
+    create(){
+
+    }
+
+    update(){
+
+    }
+
+    delete(){
+
+    }
+}
+
+module.exports = UserModel;
