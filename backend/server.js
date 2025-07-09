4d61d2144201b5a701c2d82810c1854327927ed9
Gabriel Matos <matos7771@hotmail.com>
2025-07-09 10:01:15 -0300

    allowing use of json

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const UsersRoutes = require('./routes/UsersRoutes');
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     return res.send('Online.');
