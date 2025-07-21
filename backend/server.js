7e83c4796f7db26615986cda0ca840f8b436de0e
Gabriel Matos <matos7771@hotmail.com>
2025-07-21 09:51:31 -0300

    test with query, params, body

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
     return res.send('Online.');
 });
 
+app.post('/test/:codig', (req, res) => {
+
+    const query = req.query.name;
+    const params = req.params.codig;
+    const body = req.body
+    return res.send(query + " " + params + " " + JSON.stringify(body));
+});
+
 app.use(UsersRoutes);
 
 app.listen(3000);
